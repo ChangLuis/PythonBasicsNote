c5eefddbe22b60c5dff14114d36e16e0e44960e8
ChangLuis <somebodyforluis@gmail.com>
2019-05-21 15:51:39 +0800

    Add boolean.py

---
+++ boolean.py
@@ -0,0 +1,30 @@
+#布林值
+
+boolean1 = True
+boolean2 = False
+# 要記得，python是開頭大寫，而且只有這種寫法正確
+
+type(boolean1)
+type(boolean2)
+# ans: class bool
+
+1 > 2
+# ans: False
+
+2 == 2 
+# ans: True
+
+3 < 2
+# ans: False
+
+2 != 3
+# ans: True
+
+5 >= 3
+# ans: True
+
+3 <= 5
+# ans: True
+
+# python只有 >,<,==,!=,>=,<=  沒有像其他還有甚麼三個符號的情況
+
